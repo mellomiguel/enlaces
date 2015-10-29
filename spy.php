761058e03599ebc84cf34ad4a7693f8abe5e2116
Miguel Angel <miguel.roig.m@gmail.com>
2015-10-29 17:29:44 +0100

    Commit , falta vincular hostinger y estadísticas

--- spy.php
+++ spy.php
@@ -1,42 +1,23 @@
 <?php
 
 if($_REQUEST){
-	# host | user | password | database | port
-	$mysqli = new mysqli('mysql.hostinger.es' , 'u226935810_mello', 'corneto69', 'u226935810_visit');
 
-	if(!$mysqli) {
+	//$mysqli = new mysqli('mysql.hostinger.es' , 'u226935810_mello', 'miguel', 'u226935810_visit');
+	$mysqli = new mysqli('localhost' , 'root', '', 'visits');
+
+	if(!$mysqli || $msqli->connect_errno) {
 		die ("<h3>Error Conecting to MysQl</h3>");
 	}
 	
 	$origen = $_GET['o'];
 	$destino = $_GET['d'];
-	$query = "INSERT INTO recuento VALUES($origen ,$destino)";
+	$query = "INSERT INTO recuento(origen,destino) VALUES('$origen' , '$destino');";
 
 	$result = $mysqli->query($query); 
 	
 }
 
-switch ($_GET['d']) {
-
-	case 'deviantart':
-			header('Location: http://www.deviantart.com/browse/whatshot/');
-		break;
-	
-	case 'elmundo':
-			header('Location: http://www.elmundo.es/');	
-		break;
-
-	case 'youtube':
-			header('Location: https://www.youtube.com/');	
-	break;
-
-	case 'diariodeibiza':
-			header('Location: http://www.diariodeibiza.es/');
-		break;
-
-	case 'github':
-			header('Location: https://github.com/');	
-		break;
-}
+header('Location: http://'.$_GET["d"]);
+exit;
 
 ?>
