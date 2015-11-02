3829536065850e462f5c1126859a4573a1d6f6fd
Miguel Angel <miguel.roig.m@gmail.com>
2015-11-02 17:55:58 +0100

    Estadisticas (TERMINAR)

--- estadisticas.php
+++ estadisticas.php
@@ -1,8 +1,44 @@
 <?php
 
 
+if($_REQUEST){
 
+	//$mysqli = new mysqli('mysql.hostinger.es' , 'u226935810_mello', 'miguel', 'u226935810_visit');
+	$mysqli = new mysqli('localhost' , 'root', '', 'visits');
 
+	if(!$mysqli || $msqli->connect_errno) {
+		die ("<h3>Error Conecting to MysQl</h3>");
+	}
+	
+	/*$origen = $_GET['o'];
+	$destino = $_GET['d'];
+	*/
+
+	$query1 = "SELECT COUNT(detino) FROM recuento WHERE destino = 'www.deviantart.com/browse/whatshot/' ";
+
+	$result1 = $mysqli->query($query1); 
+	//-----------------------------------------------
+
+	$query2 = "SELECT COUNT(detino) FROM recuento WHERE destino = 'www.elmundo.es/' ";
+
+	$result2 = $mysqli->query($query2); 
+	//-----------------------------------------------
+
+	$query3 = "SELECT COUNT(detino) FROM recuento WHERE destino = 'www.youtube.com/' ";
+
+	$result3 = $mysqli->query($query3); 
+	//-----------------------------------------------
+
+	$query4 = "SELECT COUNT(detino) FROM recuento WHERE destino = 'www.diariodeibiza.es/' ";
+
+	$result4 = $mysqli->query($query4); 
+
+	//-----------------------------------------------
+	$query5 = "SELECT COUNT(detino) FROM recuento WHERE destino = 'www.marca.com/' ";
+
+	$result5 = $mysqli->query($query5); 
+
+}
 
 
 
