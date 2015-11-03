d0cb9d72f0e30ade61354258efc25a2a5ff34dd9
Miguel Angel <miguel.roig.m@gmail.com>
2015-11-03 16:09:32 +0100

    Faltan Estadisticas

--- estadisticas.php
+++ estadisticas.php
@@ -14,29 +14,35 @@ if($_REQUEST){
 	$destino = $_GET['d'];
 	*/
 
-	$query1 = "SELECT COUNT(detino) FROM recuento WHERE destino = 'www.deviantart.com/browse/whatshot/' ";
+	$query1 = "SELECT COUNT(destino) AS visitas FROM recuento WHERE destino = 'www.deviantart.com/browse/whatshot/' ";
 
 	$result1 = $mysqli->query($query1); 
 	//-----------------------------------------------
 
-	$query2 = "SELECT COUNT(detino) FROM recuento WHERE destino = 'www.elmundo.es/' ";
+	$query2 = "SELECT COUNT(destino) AS visitas FROM recuento WHERE destino = 'www.elmundo.es/' ";
 
 	$result2 = $mysqli->query($query2); 
 	//-----------------------------------------------
 
-	$query3 = "SELECT COUNT(detino) FROM recuento WHERE destino = 'www.youtube.com/' ";
+	$query3 = "SELECT COUNT(destino) AS visitas FROM recuento WHERE destino = 'www.youtube.com/' ";
 
 	$result3 = $mysqli->query($query3); 
 	//-----------------------------------------------
 
-	$query4 = "SELECT COUNT(detino) FROM recuento WHERE destino = 'www.diariodeibiza.es/' ";
+	$query4 = "SELECT COUNT(destino) AS visitas FROM recuento WHERE destino = 'www.diariodeibiza.es/' ";
 
 	$result4 = $mysqli->query($query4); 
 
 	//-----------------------------------------------
-	$query5 = "SELECT COUNT(detino) FROM recuento WHERE destino = 'www.marca.com/' ";
+	$query5 = "SELECT COUNT(destino) AS visitas FROM recuento WHERE destino = 'www.marca.com/' ";
 
 	$result5 = $mysqli->query($query5); 
+
+	echo $result1;
+	echo $result2;
+	echo $result3;
+	echo $result4;
+	echo $result5;
 
 }
 
