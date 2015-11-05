ff17d21e6a6e53674c63c0d52b4d7b5f56b52343
Miguel Angel <miguel.roig.m@gmail.com>
2015-11-05 17:46:44 +0100

    Finalizado

--- spy.php
+++ spy.php
@@ -2,8 +2,8 @@
 
 if($_REQUEST){
 
-	//$mysqli = new mysqli('mysql.hostinger.es' , 'u226935810_mello', 'miguel', 'u226935810_visit' , 3306);
-	$mysqli = new mysqli('localhost' , 'root', '', 'visits');
+	$mysqli = new mysqli('mysql.hostinger.es' , 'u226935810_mello', 'miguel', 'u226935810_visit' , 3306);
+	//$mysqli = new mysqli('localhost' , 'root', '', 'visits');
 
 	if(!$mysqli || $msqli->connect_errno) {
 		die ("<h3>Error Conecting to MysQl</h3>");
